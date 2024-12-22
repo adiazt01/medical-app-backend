d6b0be60256091b099867bd21d2350f8247a7b10
adiazt01 <adiaztovar2004@gmail.com>
2024-12-22 18:22:41 -0400

    refactor medicine and therapeutic action modules; create new entities and DTOs, remove obsolete files

--- src/app/hub/hub.module.ts
+++ src/app/hub/hub.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MedicineModule } from './medicine/medicine.module';
-import { TherapeuticActionModule } from './therapeutic-action/therapeutic-action.module';
-import { PresentationModule } from './presentation/presentation.module';
-import { MainComponentModule } from './main-component/main-component.module';
 import { LaboratoryModule } from './laboratory/laboratory.module';
 import { BranchModule } from './branch/branch.module';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [AuthModule, UserModule, MedicineModule, TherapeuticActionModule, PresentationModule, MainComponentModule, LaboratoryModule, BranchModule, EmployeeModule]
+  imports: [AuthModule, UserModule, MedicineModule, LaboratoryModule, BranchModule, EmployeeModule]
 })
 export class HubModule {}
--- src/app/hub/medicine/medicine.module.ts
+++ src/app/hub/medicine/medicine.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MedicineService } from './medicine.service';
 import { MedicineController } from './medicine.controller';
+import { PresentationModule } from './presentation/presentation.module';
+import { TherapeuticActionModule } from './therapeutic-action/therapeutic-action.module';
+import { MainComponentModule } from './main-component/main-component.module';
 
 @Module({
   controllers: [MedicineController],
   providers: [MedicineService],
+  imports: [PresentationModule, TherapeuticActionModule, MainComponentModule],
 })
 export class MedicineModule {}
