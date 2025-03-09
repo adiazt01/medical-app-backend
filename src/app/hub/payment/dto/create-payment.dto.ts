6493b45cb732ac3b8fd1c3230e1d814fb3655756
Danydev <taironrojas10@gmail.com>
2025-03-09 02:56:53 -0400

    Implementado el modulo de payment, funcionando correctamente

--- src/app/hub/payment/dto/create-payment.dto.ts
+++ src/app/hub/payment/dto/create-payment.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsNumber, IsString, IsEnum } from 'class-validator';
+import { PaymentMethod, PaymentStatus } from '@prisma/client';
+
+export class CreatePaymentDto {
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsEnum(PaymentMethod)
+  @IsNotEmpty()
+  method: PaymentMethod;
+
+  @IsEnum(PaymentStatus)
+  @IsNotEmpty()
+  status: PaymentStatus;
+
+  @IsString()
+  @IsNotEmpty()
+  transactionId: string;
+}
