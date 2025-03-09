45514c21ceef68555739935b870d4d0e219a657a
adiazt01 <adiaztovar2004@gmail.com>
2025-03-09 18:41:20 -0400

    fix(main): correct environment variable for server port

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new GlobalExceptionFilter());
 
-  await app.listen(process.env.PORTp ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
