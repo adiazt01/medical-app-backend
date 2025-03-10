0ddf6af73fa123b9b62bf0b7c63e35f04e609540
adiazt01 <adiaztovar2004@gmail.com>
2025-03-09 23:54:56 -0400

    refactor(payment): remove payment module and related DTOs, services, and tests from hub module

--- src/app/hub/hub.module.ts
+++ src/app/hub/hub.module.ts
@@ -5,10 +5,9 @@ import { MedicineModule } from './medicine/medicine.module';
 import { LaboratoryModule } from './laboratory/laboratory.module';
 import { BranchModule } from './branch/branch.module';
 import { FileModule } from './file/file.module';
-import { PaymentModule } from './payment/payment.module';
 
 @Global()
 @Module({
-  imports: [AuthModule, UserModule, MedicineModule, LaboratoryModule, BranchModule, FileModule, PaymentModule]
+  imports: [AuthModule, UserModule, MedicineModule, LaboratoryModule, BranchModule, FileModule]
 })
 export class HubModule {}
